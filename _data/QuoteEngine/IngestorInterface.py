89dc63d50151423640342fbec1a5ea395c9be4b6
nixbytes <real8686@gmail.com>
2021-12-09 07:04:34 -0500

    add ingestor interface

---
+++ _data/QuoteEngine/IngestorInterface.py
@@ -0,0 +1,9 @@
+from .QuoteModel import QuoteModel
+
+class IngestorInterface():
+    """Class Ingestor."""
+    
+    def can_ingest(cls, path: str) -> bool:
+        pass
+    def parse(cls, path: str) -> List[QuoteModel]:
+        pass
--- _data/QuoteEngine/__init__.py
+++ _data/QuoteEngine/__init__.py
@@ -1,12 +1,2 @@
 from .QuoteModel import QuoteModel
 from .Ingestor import Ingestor
-
-
-class QuoteModel:
-    """Class Quote."""
-    pass
-
-
-class Ingestor:
-    """Class Ingestor."""
-    pass
