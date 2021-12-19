2aa9b62c267b2a06aa2fcbafbc2fb67e2ac38982
nixbytes <real8686@gmail.com>
2021-12-18 21:39:29 -0500

    add model for body - author

--- QuoteEngine/QuoteModel.py
+++ QuoteEngine/QuoteModel.py
@@ -1,7 +1,12 @@
+"""Represent models for QuoteModel objects
+   class represents a quote object.
+"""
+
 class QuoteModel:
     """Class Quote: Base class for a quote."""
 
     def __init__(self, body: str, author: str) -> None:
+        """Create a new object"""
         self.body = body
         self.author = author
 
