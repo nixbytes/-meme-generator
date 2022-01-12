40e9f53ff5074be3682b082408b5b777eb8e817e
nixbytes <real8686@gmail.com>
2022-01-11 19:41:34 -0500

    add the additional ingestor

--- QuoteEngine/Ingestor.py
+++ QuoteEngine/Ingestor.py
@@ -3,13 +3,16 @@
 from typing import List
 from .IngestorInterface import IngestorInterface
 from .IngestorInterface import Text_Ingestor
+from .IngestorInterface import CSV_Ingestor
+from .IngestorInterface import Docx_Ingestor
+from .IngestorInterface import PDF_Ingestor
 from .QuoteModel import QuoteModel
 
 
 class Ingestor(IngestorInterface):
     """Abstract base class for an ingestor."""
 
-    file_ingestors = [Text_Ingestor] 
+    file_ingestors = [Text_Ingestor, CSV_Ingestor, Docx_Ingestor, PDF_Ingestor] 
 
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
